73683bc5316227433ae17ec13e9a6d93d1278c75
Damindu Liyanage <damindu7@gmail.com>
2016-11-02 14:18:46 -0400

    Successfully changed to list

--- src/HeapThread.java
+++ src/HeapThread.java
@@ -1,17 +1,17 @@
-/**
- * Created by Damindu on 11/2/2016.
- */
-public class HeapThread extends Thread{
-
-    int[] array;
-    Heapsort heapsort;
-
-    public HeapThread(int[] array){
-        this.array = array;
-        this.start();
-    }
-
-    public void run(){
-        Heapsort heapsort = new Heapsort(this.array);
-    }
-}
+///**
+// * Created by Damindu on 11/2/2016.
+// */
+//public class HeapThread extends Thread{
+//
+//    int[] array;
+//    Heapsort heapsort;
+//
+//    public HeapThread(int[] array){
+//        this.array = array;
+//        this.start();
+//    }
+//
+//    public void run(){
+//        Heapsort heapsort = new Heapsort(this.array);
+//    }
+//}
--- src/Heapsort.java
+++ src/Heapsort.java
@@ -5,61 +5,69 @@ import java.util.Arrays;
  * Created by Damindu on 11/1/2016.
  */
 public class Heapsort {
-    ArrayList<Integer> array = new ArrayList();
-    public Heapsort(ArrayList<Integer> array){
-        this.array = array;
-        heapSort(this.array);
-    }
+//    ArrayList<Integer> list = new ArrayList();
+//    public Heapsort(ArrayList<Integer> list){
+//        this.list = list;
+//        heapSort(this.list);
+//    }
 
-    public void maxHeapify(ArrayList<Integer> array, int curIndex, int heapSize){
+    public static void maxHeapify(ArrayList<Integer> list, int curIndex, int heapSize){
         // Left child in heap
         int left = 2*curIndex+1;
         // Right child in heap
         int right = 2*curIndex+2;
         int largest = curIndex;
 
-        if( left < heapSize && array.get(left) > array.get(curIndex) ) {
+        if( left < heapSize && list.get(left) > list.get(curIndex) ) {
             largest = left;
         }
 
-        if( right < heapSize && array.get(right) > array.get(largest) ) {
+        if( right < heapSize && list.get(right) > list.get(largest) ) {
             largest = right;
         }
 
         if( largest != curIndex ){
-            swap(array, curIndex, largest);
-            maxHeapify(array, largest, heapSize);
+            swap(list, curIndex, largest);
+            maxHeapify(list, largest, heapSize);
         }
     }
 
-    public void buildMaxHeap(ArrayList<Integer> array, int heapSize){
+    public static void buildMaxHeap(ArrayList<Integer> list, int heapSize){
         // call maxHeapify on all internal nodes
-        int lastElementIndex = array.size() - 1;
+        int lastElementIndex = list.size() - 1;
         int parentIndex = (lastElementIndex - 1)/2;
         for(int i = parentIndex; i >= 0; i--){
-            maxHeapify(array, i, heapSize);
+            maxHeapify(list, i, heapSize);
         }
     }
 
-    public void heapSort(ArrayList<Integer> array){
-        if(array == null || array.size() < 2)
+    public static void heapSort(ArrayList<Integer> list){
+        if(list == null || list.size() < 2)
             return;
 
-        buildMaxHeap(array, array.size());
-        int heapSize = array.size();
-        for(int i = array.size() - 1; i > 0; i--){
-            swap(array, 0, i);
+        buildMaxHeap(list, list.size());
+        int heapSize = list.size();
+        for(int i = list.size() - 1; i > 0; i--){
+            swap(list, 0, i);
             heapSize = heapSize - 1;
-            maxHeapify(array, 0, heapSize);
+            maxHeapify(list, 0, heapSize);
         }
     }
-//
-//    public static void main(String[] args) {
-//        int[] array = {12, 35, 87, 26, 9, 28, 7};
-//        System.out.println("Original Array:\t\t" + Arrays.toString(array));
-//        heapSort(array);
-//        System.out.println("Sorted Array:\t\t" + Arrays.toString(array));
-//    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList();
+        list.add(12);
+        list.add(35);
+        list.add(87);
+        list.add(26);
+        list.add(9);
+        list.add(28);
+        list.add(7);
+        //int[] list = {12, 35, 87, 26, 9, 28, 7};
+        System.out.println("Original List:\t\t" + list);
+        heapSort(list);
+        System.out.println("Sorted List:\t\t" + list);
+    }
 
     /**
      * Swaps i & j list items. (counted from 0)
@@ -76,12 +84,12 @@ public class Heapsort {
         list.add(j, temp);
     }
 
- //   private void swap(ArrayList<Integer> array, int i, int j) {
-//        int tmp = array[i];
-//        array[i] = array[j];
-//        array[j] = tmp;
-//        int temp = array.get(i);
-//        array.remove(i);
+ //   private void swap(ArrayList<Integer> list, int i, int j) {
+//        int tmp = list[i];
+//        list[i] = list[j];
+//        list[j] = tmp;
+//        int temp = list.get(i);
+//        list.remove(i);
 //
 //
 //    }
